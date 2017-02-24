c4eabc66f21c59ce46951cd36aad239eb1f48378
rdr02000 <rosalesramel@gmail.com>
2017-02-25 07:55:43 +0800

    Testing

--- helloworld/app.js
+++ helloworld/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello World. This is CI");
+  res.send("Hello World.");
 });
 
 app.listen(4000);
